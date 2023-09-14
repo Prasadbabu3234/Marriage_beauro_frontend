40d26e3734497b8fc11e9e22147c8ba41981c367
prasadbabu3234 <prasadbabuyanbu@gmail.com>
2023-09-14 18:00:13 +0530

    home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,37 @@ import { useNavigate } from "react-router-dom"
 import image1 from '../../Assets/cut.png'
 import ganesh from '../../Assets/ganesh.jfif'
 import sitaram from '../../Assets/sitaram kalyanam.jfif'
+import mahalaxmi from '../../Assets/mahalaxmi.jfif'
+import corousal1 from '../../Assets/corousal1.jpg'
 import { Carousel } from 'antd';
+import { DownOutlined, SmileOutlined } from '@ant-design/icons';
+import { Dropdown, Space } from 'antd';
+import * as Icon from 'react-bootstrap-icons'
+import carousal2 from '../../Assets/elite-matchmaking-banner-2.png'
+import lastCarousal from '../../Assets/last-carousal.jpg'
+
+const items = [
+    {
+        key: '1',
+        label: (
+            <div className="d-flex justify-content-start align-items-center">
+                <input id="bridegroom" type="checkbox" name="select" value={"bridegroom"} />
+                <label htmlFor="bridegroom">Bride</label>
+            </div>
+
+        ),
+    },
+    {
+        key: '2',
+        label: (
+            <div className="d-flex justify-content-start align-items-center">
+                <input id="bridegroom" type="checkbox" name="select" value={"bridegroom"} />
+                <label htmlFor="bridegroom">Bridegroom</label>
+            </div>
+
+        ),
+    }
+];
 
 const contentStyle = {
     height: '300px',
@@ -15,7 +45,7 @@ const contentStyle = {
     alignItems: "center",
     justifyContent: "center",
     background: '#364d79',
-    borderRadius:"20px"
+    borderRadius: "20px"
 };
 
 export default function Home() {
@@ -33,51 +63,106 @@ export default function Home() {
         fetchData()
     }, [])
 
-    return <div>
+    return <div className="bottom-container">
+        <div className="mahalaxmi">
+            <img src={mahalaxmi} alt="5" className="images-1" />
+        </div>
+
         <div className="d-flex justify-content-center top-content">
-            <img src={ganesh} className="images-1" alt="img" />
-            <img src={image1} alt="image1" />
-            <img src={image1} alt="image1" />
-            <img src={image1} alt="image1" />
-            <img src={image1} alt="image1" />
-            <img src={image1} alt="image1" />
-            <img src={image1} alt="image1" />
-            <img src={sitaram} className="images-1" alt="img" />
+            <img src={ganesh} className="images-1" alt="2" />
+            <img src={image1} alt="1" />
+            <img src={image1} alt="1" />
+            <img src={image1} alt="1" />
+            <img src={image1} alt="1" />
+            <img src={image1} alt="1" />
+            <img src={image1} alt="1" />
+            <img src={sitaram} className="images-1" alt="2" />
         </div>
-        <nav>hello</nav>
+
+
+
+        <nav className="bg-success">
+            <input type="search" className="input-field" placeholder="Search..." />
+            <Dropdown
+                menu={{
+                    items,
+                }}
+            >
+                <a onClick={(e) => e.preventDefault()}>
+                    <Space style={{ color: "white" }}>
+                        Select
+                        <DownOutlined />
+                    </Space>
+                </a>
+            </Dropdown>
+            <Dropdown
+                menu={{
+                    items,
+                }}
+            >
+                <a onClick={(e) => e.preventDefault()}>
+                    <Space style={{ color: "white" }}>
+                        Caste
+                        <DownOutlined />
+                    </Space>
+                </a>
+            </Dropdown>
+
+                <Icon.PersonCircle height={30} width={30}/>
+        </nav>
         <div className="d-flex justify-content-center">
-            <marquee>BHANU PRASAD MARRIAGE BEAURO</marquee>
+            <marquee>BANU PRASAD MARRIAGE BEURO</marquee>
         </div>
-        <div className="d-flex justify-content-around align-items-center">
-            <div>
-                <h3 className="text-center">Reg No : <br></br>3FAFAFS9665L1Z7</h3>
+        <div className="d-flex justify-content-around align-items-center gap-5 flex-wrap">
+            <div className="info-content">
+                <div>
+                    <h3>Conatct Info : </h3>
+                    <p>SREE KANAKA MAHAA LAXMEE ENTERPRISES</p>
+                    <p>Contact Name : A . Prasad Babu</p>
+                    <p>Mobile : 9866335626, 9440615601</p>
+                    <span><b>Reg No : 37AFAFS9665L1Z7</b></span>
+                </div>
             </div>
-            <div style={{ width: "700px" }}>
+            <div className="corousal">
                 <Carousel autoplay>
                     <div>
-                        <img src={ganesh} alt="image" style={contentStyle} />
+                        <img src={corousal1} alt="3" style={contentStyle} />
                     </div>
                     <div>
-                        <img src={ganesh} alt="image" style={contentStyle} />
+                        <img src={"https://3.imimg.com/data3/CC/FO/MY-10157118/marriage-bureau-500x500.jpg"} alt="3" style={contentStyle} />
                     </div>
                     <div>
-                        <img src={ganesh} alt="image" style={contentStyle} />
+                        <img src={carousal2} alt="3" style={contentStyle} />
                     </div>
                     <div>
-                        <img src={ganesh} alt="image" style={contentStyle} />
+                        <img src={lastCarousal} alt="3" style={contentStyle} />
                     </div>
                 </Carousel>
             </div>
 
-            <div>
-                <h3 className="text-center">Reg No : <br></br>3FAFAFS9665L1Z7</h3>
-            </div>
-        </div>
-            <div className="d-flex justify-content-center" >
-                <div className="cards">
-                    <img src={sitaram} alt={"image"}/>
-                    <p>Sitaram</p>
+            <div className="info-content1">
+                <div>
+                    <h3>Meeting your life partner is one click away</h3>
                 </div>
             </div>
+        </div>
+        <div className="profiles" >
+            <div className="cards">
+                <img src={sitaram} alt={"4"} />
+                <p>Sitaram</p>
+            </div>
+            <div className="cards">
+                <img src={sitaram} alt={"4"} />
+                <p>Sitaram</p>
+            </div>
+            <div className="cards">
+                <img src={sitaram} alt={"4"} />
+                <p>Sitaram</p>
+            </div>
+            <div className="cards">
+                <img src={sitaram} alt={"4"} />
+                <p>Sitaram</p>
+            </div>
+        </div>
     </div>
 }
